7d49e33b18491143d1b046917fd9ec44611af931
empower87 <mjamespower87@gmail.com>
2023-02-03 21:28:20 -0800

    fixed production url

--- node_modules/client/src/App.tsx
+++ node_modules/client/src/App.tsx
@@ -46,7 +46,7 @@ function App() {
         httpBatchLink({
           url:
             process.env.NODE_ENV === "production"
-              ? "https://flow-reborn.vercel.app"
+              ? "https://flow-reborn.vercel.app/api/trpc/"
               : "http://localhost:3000/api/trpc/",
           async fetch(input, init?) {
             const fetch = getFetch()
