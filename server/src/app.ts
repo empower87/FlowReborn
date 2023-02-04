436099e093d2611fd91f31e6ceaf25ba471918e9
empower87 <mjamespower87@gmail.com>
2023-02-03 22:28:33 -0800

    last attempt at working build

--- server/src/app.ts
+++ server/src/app.ts
@@ -49,7 +49,7 @@ app.use(cookieparser())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use("/api/trpc", trpcExpress.createExpressMiddleware({ router: appRouter, createContext }))
-app.use(express.static(path.join(__dirname, "../../client/build/index.html")))
+app.use(express.static(path.join(__dirname, "../../client/build")))
 
 const PORT = customConfig.port
 
