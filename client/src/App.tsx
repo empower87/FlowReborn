6ccf7104ad9f4595df36a1ce4b7f37aebe26bf39
empower87 <mjamespower87@gmail.com>
2023-02-04 18:06:23 -0800

    lets try again with the /api/trpc to homepageurl

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -45,7 +45,9 @@ function App() {
         loggerLink(),
         httpBatchLink({
           url:
-            process.env.NODE_ENV === "production" ? "https://flow-reborn.vercel.app" : "http://localhost:5000/api/trpc",
+            process.env.NODE_ENV === "production"
+              ? "https://flow-reborn.vercel.app/api/trpc"
+              : "http://localhost:5000/api/trpc",
           headers() {
             return {
               authorization: getAuthToken(),
