e6fb4df407df25620b68d9f9817a1cc7b2899a51
empower87 <mjamespower87@gmail.com>
2023-02-14 18:53:06 -0800

    lets goooo

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -46,7 +46,7 @@ function App() {
         httpBatchLink({
           url:
             process.env.NODE_ENV === "production"
-              ? "https://flow-reborn.onrender.com"
+              ? "https://flow-reborn.onrender.com/api/trpc"
               : "http://localhost:5000/api/trpc",
           async fetch(input, init?) {
             const fetch = getFetch()
