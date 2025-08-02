01748450fd4d5492d739e7678ffe2a6fe09a73b0
Michael Power <mjamespower87@gmail.com>
2025-08-02 10:11:04 -0700

    fixed catch-all route to allow api requests to handle backend and unknon frontend routes to fall back to index.html to hopefully fix network errors on production

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -59,6 +59,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, "../../../client/build")))
 
 app.get("*", (req: Request, res: Response, next: NextFunction) => {
+  if (req.originalUrl.startsWith("/api/")) return next()
   res.sendFile(path.join(__dirname, "../../../client/build/index.html"))
 })
 
